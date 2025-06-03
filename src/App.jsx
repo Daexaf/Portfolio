93abf738d27ab9a44baaa66267725e6d91564074
Muhammad Ihsan <ihsan64.mi@gmail.com>
2025-06-03 10:14:15 +0700

    [checkpoint: adding animation loading screen]

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import "./App.css";
+import { LoadingScreen } from "./components/LoadingScreen";
+import "./index.css";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
   return (
     <>
-      <div></div>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      ></div>
     </>
   );
 }
