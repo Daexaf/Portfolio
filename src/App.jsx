a2f2ecc882efc7799cb9340c146e06f229feb30f
Muhammad Ihsan <ihsan64.mi@gmail.com>
2025-06-03 11:34:06 +0700

    [checkpoint: adding navbar for desktop and small device]

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from "react";
 import "./App.css";
 import { LoadingScreen } from "./components/LoadingScreen";
 import "./index.css";
+import Navbar from "./components/Navbar";
+import MobileMenu from "./components/MobileMenu";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
@@ -12,7 +15,10 @@ function App() {
         className={`min-h-screen transition-opacity duration-700 ${
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
-      ></div>
+      >
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      </div>
     </>
   );
 }
